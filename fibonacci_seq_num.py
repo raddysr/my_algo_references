a4d79dda507c08109104819f4acbd3db8a992d8a
Radi Radev <33155474+raddysr@users.noreply.github.com>
2021-02-04 20:50:27 +0200

    Update fibonacci_seq_num.py

--- fibonacci_seq_num.py
+++ fibonacci_seq_num.py
@@ -1,22 +1,15 @@
-def fibo_func_recu(n):
-   
-    # Check if input is 0 then it will
-    # print incorrect input
+
+def fibo_rec(n):
     if n < 0:
-        return("Incorrect input")
- 
-    # Check if n is 0
-    # then it will return 0
-    elif n == 0:
+        return"Input must be bigger than a zero!!!"
+
+    if n == 0:
         return 0
- 
-    # Check if n is 1,2
-    # it will return 1
     elif n == 1 or n == 2:
         return 1
- 
     else:
-        return fibo_def_rec(n-1) + fibo_def_rec(n-2)
+        return fibo_rec(n-1) + fibo_rec(n-2)
+    
  
 # Driver Program
 print(fibo_def_rec(10))
